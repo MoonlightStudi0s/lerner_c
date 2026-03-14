06fc30e5a6c38599b19aeed42491555612230092
MoonlightStudi0s <MoonlightStudios075@gmail.com>
2026-03-14 13:10:08 +0300

    prac01

---
+++ practice/prac01/task11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int a;
+
+void main(){
+    scanf("%d", &a);
+    if(a%2==0){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
---
+++ practice/prac01/task8.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int x = 10;
+
+void main(){
+    printf("%d\n", x++);
+    printf("%d", ++x);
+}
